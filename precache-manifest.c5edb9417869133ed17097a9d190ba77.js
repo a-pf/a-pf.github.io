32555c8337b2f200ba8f73836e7e12a0cc9b3870
Alex <32961755+howe-oh@users.noreply.github.com>
2020-05-19 22:38:26 +0100

    Initial commit

---
+++ precache-manifest.c5edb9417869133ed17097a9d190ba77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5687dc73d0ed0d680db8df7cfa9f64f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aab8da7137bba709adde",
+    "url": "/static/css/main.c078a77a.chunk.css"
+  },
+  {
+    "revision": "698be3193655aab4aa91",
+    "url": "/static/js/2.a961d0e8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a961d0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab8da7137bba709adde",
+    "url": "/static/js/main.1b4ed7a8.chunk.js"
+  },
+  {
+    "revision": "9d47a3efc4bd35c74b72",
+    "url": "/static/js/runtime-main.2f9f14fa.js"
+  }
+]);
